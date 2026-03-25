c17cb81503b476a441f1eb8d19fec1d87965948d
AidanFlynnn <adf5456@psu.edu>
2026-03-25 12:37:47 -0400

    Update news.js

--- JS/news.js
+++ JS/news.js
@@ -65,6 +65,67 @@
 //     });
 // }
 
+fetch("/JSON/articleInfo.json")
+.then(res => res.json())
+.then(data => {
+
+    const articles = data.articles;
+
+    const contentMap = {
+        "100": { title: "Local Crime Rising", image: "/images/news1.jpg", desc: "Crime increasing in neighborhoods." },
+        "110": { title: "Town Festival", image: "/images/news2.jpg", desc: "Annual event this weekend." },
+        "120": { title: "New Restaurant", image: "/images/news3.jpg", desc: "New food spot downtown." },
+
+        "200": { title: "Team Wins Championship", image: "/images/sports1.jpg", desc: "Huge victory last night." },
+        "210": { title: "Star Injured", image: "/images/sports2.jpg", desc: "Season impact expected." },
+        "220": { title: "Trade Rumors", image: "/images/sports3.jpg", desc: "Major moves coming." },
+
+        "300": { title: "Election Results", image: "/images/pol1.jpg", desc: "Big political shift." },
+        "310": { title: "New Policy", image: "/images/pol2.jpg", desc: "Government update." },
+        "320": { title: "Debate Drama", image: "/images/pol3.jpg", desc: "Leaders clash publicly." }
+    };
+
+
+    let main = contentMap[articles[0].id];
+
+    document.getElementById("mainTitle").innerText = main.title;
+    document.getElementById("mainImage").src = main.image;
+    document.getElementById("mainDesc").innerText = main.desc;
+
+
+    let left = document.getElementById("leftStories");
+    let right = document.getElementById("rightStories");
+
+    for (let i = 1; i < 3; i++) {
+        let a = contentMap[articles[i].id];
+        left.innerHTML += `<div class="side-story"><p>${a.title}</p></div>`;
+    }
+
+    for (let i = 3; i < 5; i++) {
+        let a = contentMap[articles[i].id];
+        right.innerHTML += `<div class="side-story"><p>${a.title}</p></div>`;
+    }
+
+
+    let cards = document.querySelectorAll(".container-md.border");
+
+    cards.forEach((card, index) => {
+
+        let article = articles[index];
+        let mapped = contentMap[article.id];
+
+        if (!mapped) return;
+
+        card.querySelector("img").src = mapped.image;
+        card.querySelector("h3").innerText = mapped.title;
+        card.querySelector("h4").innerText = article.author;
+        card.querySelector("h5").innerText = article.dateModified;
+        card.querySelector("p").innerText = mapped.desc;
+
+    });
+
+});
+
 $("#100").on("click", function() {
     $("#100").attr("disabled", true);
     $("#100").html("Like: 1");
